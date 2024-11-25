5c58e6b3fc7e1ebc2bb818e036f94661b7000c5b
pobo0 <muhammadfarhanixc@gmail.com>
2024-11-25 22:23:19 +0700

    Revert "menambahkan fitur rergister"
    This reverts commit ab85e06e127eb89df10bc9624b1abd51dfcd56c5.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,54 +1,32 @@
-const User = require("../models/user");
+var User = require('../models/user');
 
 module.exports = function (router) {
   // Endpoint untuk registrasi user
   router.post("/users", async function (req, res) {
     const { username, email, password } = req.body;
-    const User = require("../models/user");
 
-    module.exports = function (router) {
-      // Endpoint untuk registrasi user
-      router.post("/users", async function (req, res) {
-        const { username, email, password } = req.body;
-    
-        if (!username || !email || !password) {
-          return res.status(400).send("Please fill all required fields.");
-        }
-    
-        const newUser = new User({ username, email, password });
-    
-        try {
-          await newUser.save();
-          res.status(201).send("User created successfully!");
-        } catch (error) {
-          if (error.code === 11000) {
-            res.status(400).send("Email or Username already exists.");
-          } else {
-            res.status(500).send("Server error: " + error.message);
-          }
-        }
-      });
-    
-      return router;
-    };
-    
     if (!username || !email || !password) {
       return res.status(400).send("Please fill all required fields.");
     }
 
-    const newUser = new User({ username, email, password });
+    var user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email
+    });
 
-    try {
-      await newUser.save();
-      res.status(201).send("User created successfully!");
-    } catch (error) {
-      if (error.code === 11000) {
-        res.status(400).send("Email or Username already exists.");
-      } else {
-        res.status(500).send("Server error: " + error.message);
-      }
-    }
+    user.save()
+      .then(() => {
+        res.status(201).send('User created successfully');
+      })
+      .catch(err => {
+        if (err.code === 11000) { // Kode error untuk duplikasi
+          res.status(400).send('Username or Email already exists!!');
+        } else {
+          res.status(400).send('Error creating user: ' + err);
+        }
+      });
   });
 
   return router;
-};
+}
